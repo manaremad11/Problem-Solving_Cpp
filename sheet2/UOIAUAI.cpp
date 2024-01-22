6de67b6249fc48c6f2b07f299fbef1a16f603072
manaremad11 <85031404+manaremad11@users.noreply.github.com>
2024-01-22 22:31:20 +0200

    Create UOIAUAI.cpp

---
+++ sheet2/UOIAUAI.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main()
+{
+    char c;
+    cin>>c;
+    if(c== 'a'||c=='e'||c=='o'||c=='i'||c=='u')
+        cout<<"vowel";
+    else 
+        cout<<"consonant";
+ 
+    return 0;
+}
