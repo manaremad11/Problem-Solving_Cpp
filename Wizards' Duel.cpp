70866c6caa3f34436001e2bd7e8445128ecdcfba
manaremad11 <85031404+manaremad11@users.noreply.github.com>
2024-01-22 22:04:27 +0200

    Create Wizards' Duel.cpp

---
+++ Wizards' Duel.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double l, p, q;
+    int sum=0;
+    cin >> l >> p >> q;
+      //l=A+B;
+     //l=p*s+q*s;
+    //l=s(p+q);
+   //s=l/(p+q);
+  // A=(p*l)/(p+q);
+   cout<<(p*l)/(p+q);
+    return 0;
+}
