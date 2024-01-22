45bfba82419df43c9d212b0026bf07164a95591e
manaremad11 <85031404+manaremad11@users.noreply.github.com>
2024-01-22 21:55:45 +0200

    Create Brick.cpp

---
+++ Brick.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N=0;
+    int W=0;
+    int bricksnum=0;
+    cin >> N >> W;
+    bricksnum=N/W;
+    cout<<bricksnum;
+    return 0;
+}
